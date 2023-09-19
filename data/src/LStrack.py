0dd0985a68018a6682251ec9e958a0b4846acb84
Justin Santos <83016833+Ozzie-code-alt@users.noreply.github.com>
2023-09-19 19:58:53 +0800

    polished big and smoll c

--- data/src/LStrack.py
+++ data/src/LStrack.py
@@ -486,12 +486,12 @@ def logging_csv(number, mode, landmark_list, point_history_list):
     if mode == 0:
         pass
     if mode == 1 and (0 <= number <= 9):
-        csv_path = 'model/keypoint_classifier/keypoint.csv'
+        csv_path = 'data\src\model\keypoint_classifier\keypoint.csv'
         with open(csv_path, 'a', newline="") as f:
             writer = csv.writer(f)
             writer.writerow([number, *landmark_list])
     if mode == 2 and (0 <= number <= 9):
-        csv_path = 'model/point_history_classifier/point_history.csv'
+        csv_path = 'data\src\model\point_history_classifier\point_history.csv'
         with open(csv_path, 'a', newline="") as f:
             writer = csv.writer(f)
             writer.writerow([number, *point_history_list])
